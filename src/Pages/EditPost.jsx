491b20560f74996153fb0b6e1ee9e5a10b8af419
“akshaykumar401” <akshaykumar835103@gmail.com>
2025-05-18 17:48:31 +0530

    Refactor EditPost to display postId dynamically
    Removed unused Params import from react-router-dom and updated the EditPost component to dynamically display the postId in the UI, improving clarity and functionality.

--- src/Pages/EditPost.jsx
+++ src/Pages/EditPost.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Params, useParams} from "react-router-dom";
+import { useParams} from "react-router-dom";
 
 const EditPost = () => {
   const { postId } = useParams();
@@ -7,7 +7,7 @@ const EditPost = () => {
   return (
     <div className="md:w-[calc(100%-310px)] h-auto bg-slate-300 flex md:ml-[305px] mt-14 drop-shadow-xl dark:bg-slate-900 justify-center z-10">
       <div className="w-xl dark:bg-slate-800 dark:text-white bg-gray-300 text-black px-4 py-2 rounded-lg">
-        Edit Post
+        Edit Post: {postId}
       </div>
     </div>
   );
