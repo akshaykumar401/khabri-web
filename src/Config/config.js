5f280adc3e6ad654a33f345163336b28b732a34f
“akshaykumar401” <akshaykumar835103@gmail.com>
2025-06-24 19:47:10 +0530

    feat: update API calls to use USER_BASE_URL for login and reference token generation

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,5 +1,4 @@
 const config = {
-  BASE_URL: String(import.meta.env.VITE_BASE_URL),
   USER_BASE_URL: String(import.meta.env.VITE_USER_BASE_URL),
   POST_BASE_URL: String(import.meta.env.VITE_POST_BASE_URL),
   COMMENT_BASE_URL: String(import.meta.env.VITE_COMMENT_BASE_URL),
